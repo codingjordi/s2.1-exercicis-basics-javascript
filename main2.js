1fdc3f5b648481eda3fb5ec0d7ae6aaf655b9ab1
codingjordi <jordisaperasc@gmail.com>
2024-10-02 20:40:14 +0200

    s2.1-exercicis-basics-javascript-array-transformation-nivell-2-exercici-5

--- main2.js
+++ main2.js
@@ -40,3 +40,11 @@ const arrNumeros = [13, 7, 8, 21]
 const sumaNumeros = arrNumeros.reduce((acc, num) => acc + num, 0)
 
 console.log(sumaNumeros);
+
+// nivell 2 - exercici 5
+
+const arrNumbers = [ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]
+
+const result = arrNumbers.filter(num => num >= 10).map(num => num * 2).reduce((acc, num) => acc + num, 0)
+
+console.log(result)
