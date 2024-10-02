3c542aeb117a4501d2cb8e79abb2fa00e0695c6d
codingjordi <jordisaperasc@gmail.com>
2024-10-02 19:53:26 +0200

    s2.1-exercicis-basics-javascript-nivell-1-exercici-3

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ console.log(addNumbers(1, 2));
 const randomNumber = () => Math.floor(Math.random() * 101);
 console.log(randomNumber());
 
+// nivell 1 - exercici 3
+
+class Person {
+    constructor(nombre) {
+        this._nombre = nombre
+    }
+
+    set nombre(value) {
+        this._nombre = value;
+    }
+    
+    // nivel 1 - exercici 3
+    greeting = () => console.log(`Greeting from Mars, ${this._nombre}`);
+}
+
+const ismael = new Person('Ismael');
+
+ismael.greeting();
